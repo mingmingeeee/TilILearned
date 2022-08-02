c8632bfd8276684aca78251342c7bf269bc489f9
0613minjeong <0613minjeong@gmail.com>
2022-08-02 23:38:07 +0900

    실습 완료

--- work_algo/algo_ws_02_2/src/algo_ws_02_2/Solution.java
+++ work_algo/algo_ws_02_2/src/algo_ws_02_2/Solution.java
@@ -4,112 +4,71 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-
-	// 아래로 이동 하고 좌우 이동 가능하면 이동하고 그 다음 부터는 다시 애라로
-	// 아래
-	// 좌우
-	// 아래
-	// 반복...
-
-	// 1. 아래 검사
-	// 2. 좌우 검사 -> 한 방향만 되게 되어 있음
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-
-		st = new StringTokenizer(in.readLine());
-
-		int T = 10;
-
-		int[][] ladar = new int[100][100];
-
-		for (int i = 0; i < 100; i++) {
-			st = new StringTokenizer(in.readLine());
-			for (int j = 0; j < 100; j++) {
-				ladar[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-
-//		for (int i = 1; i <= 10; i++) {
-//			sb.append("#" + i + " ");
-//
-//			/**
-//			 * -> 출발은 항상 [0][0]~[0][99]
-//			 */
-//			
-//	
-//			for (int y = 99; y >= 0; y--) {
-//
-//				if (ladar[99][y] == 2) {
-//					
-//					int testX = 99;
-//					int testY = y;
-//					while (true) {
-//						for (int k = 0; k < dir_x.length; k++) {
-//							
-//							while (testX >= 0 && testX < 100 && testY >= 0 && testY < 100) {
-//
-//								if (ladar[testX][testY] == 1 || ladar[testX][testY] == 2) {
-//									ladar[testX][testY] = 3;
-//									testX += dir_x[k];
-//									testY += dir_y[k];
-//								} else {
-//									break;
-//								}
-//
-//							}
-//						}
-//
-//						if (testX == 0) {
-//							System.out.println(y);
-//							return;
-//						}
-//					
-//					}
-//
-//				}
-
-		for (int y = 0; y < 100; y++) {
-			int testX = 0;
-			int testY = y;
-			System.out.println(testX + " " + testY);
-			// 1) 아래로 가다가 좌우 만나면 좌우로
-			while (testX >= 0 && testX < 100 && testY >= 0 && testY < 100 && ladar[testX][testY] == 1) {
-
-				// 우로
-				if (testX >= 0 && testX < 100 && testY + 1 < 100 && ladar[testX][testY + 1] == 1) {
-					System.out.println(testX + " " + testY);
-					while (testY + 1 < 100 && ladar[testX][testY + 1] == 1) {
-						if (ladar[testX][testY + 1] == 1) {
-							ladar[testX][testY + 1] = 3;
-							testY++;
-						}
-					} // 좌로
-				} else if (testX >= 0 && testX < 100 && testY - 1 >= 0 && ladar[testX][testY - 1] == 1) {
-					System.out.println(testX + " " + testY);
-					while (testY - 1 >= 0 && ladar[testX][testY - 1] == 1) {
-						if (ladar[testX][testY - 1] == 1) {
-							ladar[testX][testY - 1] = 3;
-							testY--;
-						}
-					}
-				}
-				// 처음에는 아래로
-				else {
-					ladar[testX + 1][testY] = 3;
-					testX++;
-					}
-				if (ladar[testX][testY] == 2) {
-					System.out.println(y);
-				}
-				
-			}
-
-		}
-
-	}
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = 10;
+        int N = 100;
+ 
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            sb.append("#" + test_case + " ");
+ 
+            String string = in.readLine();
+ 
+            // 배열 입력
+            int[][] data = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(in.readLine(), " ");
+                for (int j = 0; j < N; j++) {
+                    data[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+ 
+            // 알고리즘
+            int[] dx = { 0, 0, -1 };
+            int[] dy = { -1, 1, 0 };
+ 
+            int x = N-1;
+            int y = 0;
+            for (int i = 0; i < N; i++) {
+                if (data[x][i] == 2) {
+                    y = i;
+                    break;
+                }
+            }
+             
+            while (true) {
+                 
+                for (int i = 0; i < 3; i++) {
+                    int testX = x + dx[i];
+                    int testY = y + dy[i];
+                    if (testX >= 0 && testX < N && testY >= 0 && testY < N 
+                            && data[testX][testY] == 1) {
+                        x += dx[i];
+                        y += dy[i];
+                        data[testX][testY] = 3;
+                    }else {
+                        continue;
+                    }
+ 
+                }
+                 
+                if(x==0) {
+                    sb.append(y + "\n");
+                    break;
+                }
+ 
+            }
+ 
+        }
+        // 출력
+        System.out.println(sb);
+ 
+    }
+ 
 }
--- work_algo/algo_ws_02_3/src/algo_ws_02_3/Solution.java
+++ work_algo/algo_ws_02_3/src/algo_ws_02_3/Solution.java
@@ -3,73 +3,73 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-
-	public static void main(String[] args) throws Exception {
-
-		/**
-		 * 0. 입력파일 읽어들이기
-		 */
-
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-
-		/**
-		 * 1. 입력 파일 객체화
-		 */
-		int T;
-		T = Integer.parseInt(in.readLine());
-		
-		for (int test_case = 1; test_case <= T; test_case++) {
-			sb.append("#" + test_case + " ");
-			
-			int N = Integer.parseInt(in.readLine());
-			
-			int[][] data  = new int[N][N];
-			for (int i = 0; i < N; i++) {
-				String[] split = in.readLine().split("");
-				for (int j = 0; j < N; j++) {
-					data[i][j] = Integer.parseInt(split[j]);
-				}
-			}
-			
-			/**
-    		 * 2. 알고리즘 풀기
-    		 */
-			
-			boolean isIncrease = true;
-			int target = N/2;
-			
-			int sum = 0;
-			int i = 0;
-			int k = 0;
-			while(k < N) {
-				
-				for(int l=N/2-i; l<2*i+1; l++) {
-					sum += data[k][l];
-				}
-				
-				
-				if(isIncrease)
-					i++;
-				else
-					i--;
-					
-				if(target == i)
-					isIncrease = false;
-
-				k++;
-			}
-	
-			
-			sb.append(sum).append("\n");
-			
-		}
-		/**
-		 * 3. 정답 출력
-		 */
-		System.out.println(sb);
-	}
+ 
+    public static void main(String[] args) throws Exception {
+ 
+        /**
+         * 0. 입력파일 읽어들이기
+         */
+ 
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+ 
+        /**
+         * 1. 입력 파일 객체화
+         */
+        int T;
+        T = Integer.parseInt(in.readLine());
+         
+        for (int test_case = 1; test_case <= T; test_case++) {
+            sb.append("#" + test_case + " ");
+             
+            int N = Integer.parseInt(in.readLine());
+             
+            int[][] data  = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                String[] split = in.readLine().split("");
+                for (int j = 0; j < N; j++) {
+                    data[i][j] = Integer.parseInt(split[j]);
+                }
+            }
+             
+            /**
+             * 2. 알고리즘 풀기
+             */
+             
+            boolean isIncrease = true;
+            int target = N/2;
+             
+            int sum = 0;
+            int i = 0;
+            int k = 0;
+            while(k < N) {
+                 
+                int tmp = N/2-i;
+                for(int l=tmp; l<tmp+2*i+1; l++) {
+                    sum += data[k][l];
+                }
+ 
+                if(isIncrease)
+                    i++;
+                else
+                    i--;
+                     
+                if(target == i)
+                    isIncrease = false;
+ 
+                k++;
+            }
+     
+             
+            sb.append(sum).append("\n");
+             
+        }
+        /**
+         * 3. 정답 출력
+         */
+        System.out.println(sb);
+    }
 }
