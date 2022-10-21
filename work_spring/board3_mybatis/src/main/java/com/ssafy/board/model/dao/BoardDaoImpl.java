18aa4072aa66ddf35a63ddc89782b52cb8941b08
0613minjeong <0613minjeong@gmail.com>
2022-10-22 03:20:49 +0900

    board완성
    Former-commit-id: 51bd1448529f507c2a87043ab607ce3b29e9b718

--- work_spring/board3_mybatis/src/main/java/com/ssafy/board/model/dao/BoardDaoImpl.java
+++ work_spring/board3_mybatis/src/main/java/com/ssafy/board/model/dao/BoardDaoImpl.java
@@ -1,6 +1,7 @@
 package com.ssafy.board.model.dao;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,6 +9,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.board.model.BoardDto;
+import com.ssafy.board.model.FileInfoDto;
 import com.ssafy.util.SqlMapConfig;
 
 @Repository
@@ -22,6 +24,13 @@ public class BoardDaoImpl implements BoardDao {
 
 		try (SqlSession sqlSession = SqlMapConfig.getSqlSession()) {
 			sqlSession.insert(NAMESPACE + "writeArticle", boardDto);
+			articleNo = sqlSession.selectOne(NAMESPACE + "getLastNo"); 
+			
+			Map<String,Object> map = new HashMap <String,Object>();
+			map.put("list", boardDto.getFileInfos());
+			map.put("articleNo", articleNo);
+			
+			sqlSession.insert(NAMESPACE + "uploadFiles", map);
 			sqlSession.commit();
 		}
 
@@ -31,36 +40,53 @@ public class BoardDaoImpl implements BoardDao {
 	@Override
 	public List<BoardDto> listArticle(Map<String, Object> map) throws SQLException {
 
+		// map: pgno, key, word
 		try (SqlSession sqlSession = SqlMapConfig.getSqlSession()) {
-			sqlSession.selectList(NAMESPACE + "listArticle", map);
+			return sqlSession.selectList(NAMESPACE + "listArticle", map);
 		}
 
-		return null;
 	}
 
 	@Override
 	public int getTotalArticleCount(Map<String, String> map) throws SQLException {
-		return 0;
+		try(SqlSession sqlSession = SqlMapConfig.getSqlSession()) {
+			return sqlSession.selectOne(NAMESPACE + "getTotalArticleCount", map);
+		}
 	}
 
 	@Override
 	public BoardDto getArticle(int articleNo) throws SQLException {
-		return null;
+		try(SqlSession sqlSession = SqlMapConfig.getSqlSession()) {
+			BoardDto boardDto = sqlSession.selectOne(NAMESPACE + "getArticle", articleNo);
+			List<FileInfoDto> list = sqlSession.selectList(NAMESPACE + "getFileInfos", articleNo);
+			boardDto.setFileInfos(list);
+			return boardDto;
+		}
 	}
 
 	@Override
 	public void updateHit(int articleNo) throws SQLException {
-
+		try(SqlSession sqlSession = SqlMapConfig.getSqlSession()) {
+			sqlSession.update(NAMESPACE + "updateHit", articleNo);
+			sqlSession.commit();
+		}
 	}
 
 	@Override
 	public void modifyArticle(BoardDto boardDto) throws SQLException {
-
+		try(SqlSession sqlSession = SqlMapConfig.getSqlSession()) {
+			sqlSession.update(NAMESPACE + "modifyArticle", boardDto);
+			sqlSession.commit();
+		}
 	}
 
 	@Override
 	public void deleteArticle(int articleNo) throws SQLException {
-
+		try(SqlSession sqlSession = SqlMapConfig.getSqlSession()){
+			sqlSession.delete(NAMESPACE + "deleteFile", articleNo);
+			sqlSession.delete(NAMESPACE + "deleteArticle", articleNo);
+			sqlSession.commit();
+		}
 	}
 
 }
