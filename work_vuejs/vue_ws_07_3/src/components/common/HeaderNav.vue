d76fc33a3298735436730187bc5b8d8ad8efcef9
0613minjeong <0613minjeong@gmail.com>
2022-11-11 15:50:50 +0900

    1111
    Former-commit-id: 2511f5abc0930b0f8c3f7e31e36d46e2d1d53c61

--- work_vuejs/vue_ws_07_3/src/components/common/HeaderNav.vue
+++ work_vuejs/vue_ws_07_3/src/components/common/HeaderNav.vue
@@ -2,11 +2,11 @@
   <div class="header">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/book">도서목록</router-link>
+      <router-link to="/book/list">도서목록</router-link>
     </nav>
-    <router-link to="/">
+    <a href="#">
       <img src="@/assets/ssafy_logo.png" class="ssafy_logo" />
-    </router-link>
+    </a>
     <p class="logo">도서관리</p>
   </div>
 </template>
