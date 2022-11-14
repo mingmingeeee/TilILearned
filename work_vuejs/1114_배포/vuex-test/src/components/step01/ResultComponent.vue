ea0d7a5575122e4b9d8716ae27d322900701221c
0613minjeong <0613minjeong@gmail.com>
2022-11-14 17:56:53 +0900

    1114
    Former-commit-id: d416822d319f7cbed091e0726f0dde8ff9e61820

---
+++ work_vuejs/1114_배포/vuex-test/src/components/step01/ResultComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>{{ total }}</h2>
+</template>
+
+<script>
+export default {
+  // props: ["total"],
+  computed: {
+    total() {
+      return this.$store.state.count; // 미리 store에 있던 거 들고 와서, 값 바뀌면 재계산
+    },
+  },
+};
+</script>
+
+<style></style>
